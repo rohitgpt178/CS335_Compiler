fd9cc59490397f95681980978ac2fde2b41190bf
Rohit Gupta <rohitgpt@iitk.ac.in>
2018-01-28 02:54:21 +0530

    commit

---
+++ asgn1/test/test3.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	var d int
+	a, b := c, d
+	b := [5]int{
+		1,
+		2,
+		3,
+		4,
+		5,
+	}
+	for i := 0; i < 2; i++ {
+		kk := 0
+	}
+
+	if k := 0; k != 1 {
+		j := 0
+	}
+
+	for _, arg := range flag.Args() {
+		if v, err := strconv.Atoi(arg); err == nil && len(arg) == 1 {
+			numdays = v
+		} else {
+			location = arg
+		}
+	}
+}
